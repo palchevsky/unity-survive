040f4b521f397256cc426d6f884e2d6d09571931
Andrew <palchevsky_sam@outlook.com>
2019-01-20 23:52:59 +0300

    Initial

---
+++ Assets/Scripts/Input/InputController.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+
+namespace Survive
+{
+    public class InputController : MonoBehaviour
+    {
+        #region Variables
+        [SerializeField]
+        private readonly float _minThreshold;
+
+        private static InputController instance;
+
+        private bool _isTouchMoving;
+        private Vector3 _moveDirection;
+        private Vector3 _startPosition;
+        private Vector3 _currentPosition;
+        #endregion
+
+        #region Properties
+        public static InputController Instance => instance;
+
+        public Vector3 MoveDirection => _moveDirection;
+
+        public float turnSpeed = 50f;
+        public float moveSpeed = 10f;
+        #endregion
+
+        #region Unity Lifecycle
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                DestroyImmediate(gameObject);
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.touchSupported)
+            {
+                if (Input.touches.Length > 0)
+                {
+                    if (Input.touches[0].phase == TouchPhase.Began)
+                    {
+                        _isTouchMoving = true;
+                        _startPosition = Input.touches[0].position;
+                    }
+                    else if (Input.touches[0].phase == TouchPhase.Ended)
+                    {
+                        _isTouchMoving = false;
+                    }
+
+                    _currentPosition = Input.touches[0].position;
+                }
+            }
+            else
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    _isTouchMoving = true;
+                    _startPosition = Input.mousePosition;
+                }
+                else if (Input.GetMouseButtonUp(0))
+                {
+                    _isTouchMoving = false;
+                }
+
+                _currentPosition = Input.mousePosition;
+            }
+
+            if (_isTouchMoving)
+            {
+                if (Vector3.Distance(_currentPosition, _startPosition) > _minThreshold)
+                {
+                    Vector3 direction = (_currentPosition - _startPosition).normalized;
+
+                    if (direction != Vector3.zero)
+                    {
+                        _startPosition = _currentPosition;
+                        _moveDirection = direction;
+                    }
+                }
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                Vector3 direction = (Vector3.forward + _startPosition).normalized;
+
+                if (direction != Vector3.zero)
+                {
+                    _startPosition += Vector3.forward;
+                    _moveDirection = direction;
+                }
+
+
+                //transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            }
+
+            else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                Vector3 direction = (-_startPosition).normalized;
+
+                if (direction != Vector3.zero)
+                {
+                    _startPosition -= Vector3.forward;
+                    _moveDirection = direction;
+                }
+
+
+                //transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+            }
+
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                Vector3 direction = (Vector3.up - _startPosition).normalized;
+
+                if (direction != Vector3.zero)
+                {
+                    _startPosition -= Vector3.up;
+                    _moveDirection = direction;
+                }
+
+
+//                transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+            }
+
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                Vector3 direction = (Vector3.up + _startPosition).normalized;
+
+                if (direction != Vector3.zero)
+                {
+                    _startPosition += Vector3.up;
+                    _moveDirection = direction;
+                }
+
+                //transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+            }
+            else
+            {
+                _moveDirection = Vector3.zero;
+            }
+
+        }
+        #endregion    
+    }
+}
---
+++ Assets/Scripts/Player/MoveController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Survive
+{
+    public class MoveController : MonoBehaviour
+    {
+        #region Variables
+        [SerializeField]
+        private float _speed;
+        [SerializeField]
+        private float _turnSpeed;
+        #endregion
+
+        #region Unity Lifecycle
+        private void Update()
+        {
+            Vector3 direction = new Vector3(InputController.Instance.MoveDirection.x, 0, InputController.Instance.MoveDirection.y);
+
+            transform.position += direction * _speed;
+
+            //transform.Translate(direction * _speed * Time.deltaTime);
+
+            Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+            transform.rotation = rotation;
+        }
+        #endregion
+    }
+}
